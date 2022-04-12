2725f53ffc77cde7474d68a98c8858916f754a73
guilherme <guilherme_scherer@outlook.com>
2022-04-11 22:06:36 -0300

    Initial project structure

---
+++ back-end/src/test/java/com/personal/productcatalog/ProductCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.productcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
