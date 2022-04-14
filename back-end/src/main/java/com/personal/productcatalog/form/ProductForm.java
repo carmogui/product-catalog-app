4827221f40d142281ae98f01cf02b94480cfdeb0
guilherme <guilherme_scherer@outlook.com>
2022-04-13 21:27:55 -0300

    Create a initial structure to product

---
+++ back-end/src/main/java/com/personal/productcatalog/form/ProductForm.java
@@ -0,0 +1,17 @@
+package com.personal.productcatalog.form;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+public class ProductForm {
+
+    @NotNull
+    @NotEmpty
+    private String name;
+    @NotNull
+    private Integer stock;
+    @NotNull
+    private BigDecimal price;
+
+}
